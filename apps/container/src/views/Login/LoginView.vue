94b54cc95f06317b00f29d7a1e3a963ac3ac37e6
Joabesv <joabevarjao123@gmail.com>
2026-06-13 09:38:01 -0300

    remove logger debug

--- apps/container/src/views/Login/LoginView.vue
+++ apps/container/src/views/Login/LoginView.vue
@@ -2,16 +2,10 @@
 import { computed } from 'vue';
 
 import { buildGoogleAuthUrl, isLocalAppSession } from '@/utils/runtimeConfig';
-import { logger } from '@/utils/logger'
 
 const googleAuthUrl = computed(() =>
   buildGoogleAuthUrl({ requesterKey: 'ufabc-next' }),
 );
-
-logger.info({
-  data: googleAuthUrl.value,
-  me: 'Joabe',
-}, 'user is starting loggin in dev env')
 </script>
 
 <template>
